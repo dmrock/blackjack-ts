ccaf886c856f9c8a8a6bc552eb44dfa83e072160
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-15 22:59:00 +0200

    feat: utils draft

---
+++ utils.ts
@@ -0,0 +1,13 @@
+import { ICard } from './types';
+
+export function getDecision(): 'hit' | 'stand' {}
+
+export function getHandValue(cards: ICard[]): number {}
+
+export function shuffleArray<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
