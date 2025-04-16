79644ee5c2073a78e171f180815b291c6c6c6f71
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-17 00:05:08 +0200

    feat: implement Deck class

--- deck.ts
+++ deck.ts
@@ -4,8 +4,27 @@ import { shuffleArray } from './utils';
 
 class Deck implements IDeal {
   private deck: Card[] = [];
-  reset() {}
-  deal(num: number): Card[] {}
+
+  reset() {
+    this.deck = [];
+    for (let suit in Suit) {
+      for (let value = 1; value <= 13; value++) {
+        this.deck.push(new Card(value, Suit[suit as keyof typeof Suit]));
+      }
+    }
+    shuffleArray(this.deck);
+  }
+
+  deal(num: number): Card[] {
+    const cards: Card[] = [];
+    for (let i = 0; i < num; i++) {
+      const card = this.deck.pop();
+      if (card) {
+        cards.push(card);
+      }
+    }
+    return cards;
+  }
 }
 
 export default Deck;
