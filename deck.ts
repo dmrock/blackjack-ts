715ffa97c885ba4ed2dc25243d4302b16a844629
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-04-17 20:16:32 +0200

    feat: enhance Deck class constructor and reset method formatting

--- deck.ts
+++ deck.ts
@@ -5,24 +5,34 @@ import { shuffleArray } from './utils';
 class Deck implements IDeal {
   private deck: Card[] = [];
 
+  constructor() {
+    this.reset();
+  }
+
   reset() {
     this.deck = [];
+
+    // Create a new deck of cards
     for (let suit in Suit) {
       for (let value = 1; value <= 13; value++) {
         this.deck.push(new Card(value, Suit[suit as keyof typeof Suit]));
       }
     }
+
+    // Shuffle the deck
     shuffleArray(this.deck);
   }
 
   deal(num: number): Card[] {
     const cards: Card[] = [];
+
     for (let i = 0; i < num; i++) {
       const card = this.deck.pop();
       if (card) {
         cards.push(card);
       }
     }
+
     return cards;
   }
 }
